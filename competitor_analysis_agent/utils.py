b96f281caec603c9e354d8e7847d698853834baf
Ayush-Pujari-07 <ayush08.pujari@gmail.com>
2024-02-20 11:45:52 +0530

    added generate_metadata()

--- competitor_analysis_agent/utils.py
+++ competitor_analysis_agent/utils.py
@@ -2,6 +2,7 @@ import gc
 import sys
 import tiktoken
 
+from llm_manager.openai_manager import openai_manager
 from competitor_analysis_agent.logger import logger
 from competitor_analysis_agent.exception import CustomException
 
@@ -21,3 +22,20 @@ def token_count(string: str, encoding_name: str = "cl100k_base") -> int:
 
 def garbage_collector():
     gc.collect()
+
+
+def generate_metadata(metadata):
+    document = []
+    ids = []
+    metadatas = []
+    embeddings = []
+    for index, data in enumerate(metadata):
+        embeddings.append(openai_manager.client.embeddings.create(
+            input=data['content'],
+            model="text-embedding-3-small"
+        ).data[0].embedding)
+        document.append(data['content'])
+        ids.append(f"id{index+1}")
+        metadatas.append({"source":data['title']})
+    
+    return document, ids, metadatas, embeddings
